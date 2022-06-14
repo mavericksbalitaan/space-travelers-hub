f72e071310df60fb2b50c594528da1c6997c443f
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-06-14 17:33:48 +0500

    Show no rockets/missions if list is empty

--- src/components/myprofile/Myprofile.js
+++ src/components/myprofile/Myprofile.js
@@ -4,6 +4,24 @@ import '../stylesheets/Myprofile.css';
 
 function Myprofile() {
   const rockets = useSelector((state) => state.rockets.filter((el) => el.rocketReserved === true));
+  if (!rockets.length) {
+    return (
+      <section className="tableContainer">
+        <div className="missions">
+          <h1>My Missions</h1>
+          <ul className="itemsList">
+            <li>There are no missions joined.</li>
+          </ul>
+        </div>
+        <div className="rockets">
+          <h1>My Rockets</h1>
+          <ul className="itemsList">
+            <li>There are no rockets reserved.</li>
+          </ul>
+        </div>
+      </section>
+    );
+  }
   return (
     <section className="tableContainer">
       <div className="missions">
