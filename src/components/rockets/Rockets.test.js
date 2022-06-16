d5ce37d11ff0aab2e194e559312082dd650ea120
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-06-16 16:04:21 +0500

    Write additional test for rockets component

--- src/components/rockets/Rockets.test.js
+++ src/components/rockets/Rockets.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Rockets from './Rockets';
 import store from '../../redux/configureStore';
+import App from '../../App';
 
+// rockets snapshots
 const MockRockets = () => (
   <Provider store={store}>
     <Rockets />
@@ -13,4 +18,23 @@ const MockRockets = () => (
 test('renders correctly', () => {
   const tree = renderer.create(<MockRockets />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+// test rockets link from the app section
+const MockApp = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+describe('when you click the rockets link', () => {
+  test('should render rockets page content', () => {
+    render(
+      (<MockApp />),
+    );
+    userEvent.click(screen.getByRole('link', { name: /rockets/i }));
+    expect(screen.getByText(/rockets/i)).toBeInTheDocument();
+  });
 });
